3779e198e2c63d7bfb6a723959028677f5c5fc24
humnalodhi <humna02468@gmail.com>
2023-04-06 08:18:17 +0500

    first commit

---
+++ app/src/main/java/com/example/mad_task_07/OrderActivity.java
@@ -0,0 +1,23 @@
+package com.example.mad_task_07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.mad_task_07.R;
+
+import java.sql.Array;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+    }
+}
